4f224334577e51639e40e8d1f4789768037f6e5b
Dag Wästberg <dwastberg@gmail.com>
2025-02-28 13:56:34 +0100

    add test

--- tests/test_traverse.py
+++ tests/test_traverse.py
@@ -22,6 +22,20 @@ def mesh_planes():
     return Mesh(vertices, faces)
 
 
+def mesh_N_planes(n=1):
+    vertice = []
+    faces = []
+    for z in range(n):
+        vertice += [[0, 0, z], [1, 0, z], [1, 1, z], [0, 1, z]]
+        faces += [
+            [0 + (z * 4), 1 + (z * 4), 2 + (z * 4)],
+            [2 + (z * 4), 3 + (z * 4), 0 + (z * 4)],
+        ]
+    vertices = np.array(vertice)
+    faces = np.array(faces)
+    return Mesh(vertices, faces)
+
+
 def test_traverse_all():
     m = mesh_planes()
     m.build("medium")
@@ -50,6 +64,34 @@ def test_traverse():
     assert 1 in travesed
     assert 2 not in travesed
     assert 3 not in travesed
+
+
+def test_traverse_large():
+    m = mesh_N_planes(500)
+    m.build("medium")
+    ray_origin = [0.5, 0.5, 249.5]
+    ray_direction = [0, 0, -1]
+
+    travesed = []
+    for t_id in m.traverse(ray_origin, ray_direction):
+        travesed.append(t_id)
+    assert len(travesed) == 500
+    for i in range(500):
+        assert i in travesed
+
+
+def test_traverse_large_r():
+    m = mesh_N_planes(500)
+    m.build("medium")
+    ray_origin = [0.5, 0.5, 249.5]
+    ray_direction = [0, 0, 1]
+
+    travesed = []
+    for t_id in m.traverse(ray_origin, ray_direction):
+        travesed.append(t_id)
+    assert len(travesed) == 500
+    for i in range(500, 1000):
+        assert i in travesed
 
 
 def test_traverse_all_r():
